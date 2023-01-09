a501eec8c19efa1891b4f48872b275e903974ab7
L-ItsLocked <L-itslocked@users.noreply.github.com>
2023-01-08 22:39:12 -0500

    Adjust weather icon styling and placement

--- src/WeatherResults.js
+++ src/WeatherResults.js
@@ -6,19 +6,23 @@ export default function WeatherResults(props) {
   return (
     <div className="WeatherResults">
       <div className="row-md-12 Weather">
-        <img
-          src={props.data.iconURL}
-          alt={props.data.description}
-          className="text-capitalize"
-        />
-
         <h3>- {props.data.city} -</h3>
-
-        <ul>
-          <li>Temperature: {props.data.temperature}°F</li>
-          <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind: {props.data.wind} mph</li>
-        </ul>
+        <div className="row">
+          <div className="col-7">
+            <ul>
+              <li>Temperature: {props.data.temperature}°F</li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind} mph</li>
+            </ul>
+          </div>
+          <div className="col-5 float-left">
+            <img
+              src={props.data.iconURL}
+              alt={props.data.description}
+              className="text-capitalize"
+            />
+          </div>
+        </div>
         <p>
           <DateAndTime date={props.data.date} />
         </p>
