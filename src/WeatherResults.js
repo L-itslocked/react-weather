3d102f99161ae466d486869d5265443b511f9f17
L-ItsLocked <L-itslocked@users.noreply.github.com>
2023-01-08 22:31:40 -0500

    Change WeatherResults layout

--- src/WeatherResults.js
+++ src/WeatherResults.js
@@ -1,38 +1,24 @@
 import React from "react";
 import DateAndTime from "./DateAndTime.js";
+import "./WeatherResults.css";
 
 export default function WeatherResults(props) {
   return (
     <div className="WeatherResults">
-      <div className="Weather">
-        <h3>
-          <span>
-            <a alt="waving hand" />
-            👋
-          </span>{" "}
-          Hi {props.data.city}!
-        </h3>
-        <div className="row">
-          <div className="col-md-12 weather-icon">
-            <img
-              src={props.data.icon}
-              alt={props.data.description}
-              className="text-capitalize"
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-8">
-            <ul>
-              <li>Temperature: {props.data.temperature}°F</li>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} mph</li>
-            </ul>
-            <div className="col-md-3 globe-image">
-              <img src="images/globe.svg" alt="{city weather conditions}" />
-            </div>
-          </div>
-        </div>
+      <div className="row-md-12 Weather">
+        <img
+          src={props.data.iconURL}
+          alt={props.data.description}
+          className="text-capitalize"
+        />
+
+        <h3>- {props.data.city} -</h3>
+
+        <ul>
+          <li>Temperature: {props.data.temperature}°F</li>
+          <li>Humidity: {props.data.humidity}%</li>
+          <li>Wind: {props.data.wind} mph</li>
+        </ul>
         <p>
           <DateAndTime date={props.data.date} />
         </p>
