e33024785d32b9ebbb4c252272e82bb9ed83d019
L-ItsLocked <L-itslocked@users.noreply.github.com>
2023-01-15 21:12:19 -0500

    Adjust footer/navbar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,7 +9,7 @@ root.render(
     <div className="container">
       <App />
     </div>
-    <nav className="navbar sticky-bottom bg-body-tertiary">
+    <nav className="navbar fixed-bottom">
       <div className="container-fluid">
         <footer>
           <a
@@ -20,14 +19,17 @@ root.render(
           >
             Open-source code on GitHub{" "}
           </a>{" "}
+          and{" "}
+          <a
+            href="https://dapper-tiramisu-226c4a.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>{" "}
           by Laura Doyon
         </footer>
       </div>
     </nav>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
