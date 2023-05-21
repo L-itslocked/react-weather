00210a9f939100097add2762b2f0b89fc184735d
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-05-21 02:28:32 -0400

    Add socials to footer

--- src/index.js
+++ src/index.js
@@ -28,6 +28,18 @@ root.render(
             hosted on Netlify
           </a>{" "}
           by Laura Doyon
+          <div>
+            {" "}
+            <a href="https://www.linkedin.com/in/doyonlaura">
+              <i class="fa-brands fa-linkedin coder-social"></i>
+            </a>
+            <a href="https://github.com/L-itslocked">
+              <i class="fa-brands fa-github coder-social"></i>
+            </a>
+            <a href="https://codepen.io/Itslocked" target="_blank">
+              <i class="fa-brands fa-codepen coder-social"></i>
+            </a>
+          </div>
         </footer>
       </div>
     </nav>
